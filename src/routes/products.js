9cc96a2c9cb44a921566f5a5599460823cea9e4f
Prasad Chandima <159484298+pcSenavirathna@users.noreply.github.com>
2025-08-28 22:40:02 +0530

    get all products in the databse

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/upload');
 const productController = require('../controllers/productController');
+const Product = require('../models/Product');
 
 // Use 'images' as the field name, allow multiple files
 router.post('/', upload.array('images', 10), productController.createProduct);
+
+// Get total product count
+router.get('/count', async (req, res) => {
+	try {
+		const count = await Product.countDocuments();
+		res.json({ count });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+// Get all products
+router.get('/', async (req, res) => {
+	try {
+		const products = await Product.find();
+		res.json(products);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
 
 module.exports = router;
